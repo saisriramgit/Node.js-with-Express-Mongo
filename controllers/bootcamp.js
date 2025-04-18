1999bbe2e0d6cd23f78b7a97927325507a02c4bd
saisriramgit <gssriram1999@gmail.com>
2025-04-18 16:20:33 -0400

    nodejs api with mongo and mongoose

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -22,9 +22,14 @@ exports.getBootcamps = async (req, res, next) => {
 exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return next(
+        new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404)
+      );
+    }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
